b8789d8d3ab24cabae8b642494d639b22da3b44f
GURPARKASH SINGH <gpssohi@cse.iitb.ac.in>
2018-02-16 01:40:46 +0530

    Regular Update 8

--- assignments/satan_cantor/solution.pl
+++ assignments/satan_cantor/solution.pl
@@ -34,3 +34,53 @@ sum_of_n(N,K,[A|L]) :-
 first_elem(K,K,K) :- !.
 first_elem(A,_,A).
 first_elem(A,K,B) :- C is A+1, first_elem(C,K,B).
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part (b)
+
+
+satan_cantor_infi(A) :-
+    sci_h(1,A).
+
+sci_h(N,A) :-
+    sub_routine_n(N,A).
+sci_h(N,A) :-
+    N1 is N+1,
+    sci_h(N1,A).
+
+sub_routine_n(N,A) :-
+    srn_h(N,A,1).
+
+srn_h(N,A,N) :-
+    sum_upto_n(N,A).
+srn_h(N,A,K) :-
+    K < N,
+    sum_of_n(K,N,A).
+srn_h(N,A,K) :-
+    K < N,
+    K1 is K+1,
+    srn_h(N,A,K1).
+
+sum_upto_n(N,A) :-
+    sun_h(N,A,0).
+
+sun_h(N,A,N) :-
+    sum_of_n(N,N,A).
+sun_h(N,A,K) :-
+    K < N,
+    sum_of_n(N,K,A).
+sun_h(N,A,K) :-
+    K < N,
+    K1 is K+1,
+    sun_h(N,A,K1).    
+    
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part (c)
+
+
+    
+    
+    
+    
+    
+    
